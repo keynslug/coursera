086f8d38a976e93725fab4c448f531982968f99c
Andrew Majorov <encube.ul@gmail.com>
2013-07-16 16:37:29 +0400

    crypto-007: week 3 first working solution

---
+++ crypto-007/w3/jigsaw.hs
@@ -0,0 +1,28 @@
+-- Jigsaw
+
+import Data.Char
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as ByteString
+
+import System.IO
+import System.Environment (getArgs)
+
+import Data.Digest.Pure.SHA
+
+----
+
+getChainHash :: (ByteString -> Digest h) -> Integer -> ByteString -> Digest h
+getChainHash hash block bs
+    | len > bl  = chain $ ByteString.splitAt bl bs
+    | otherwise = hash bs where
+        bl             = fromIntegral block
+        len            = ByteString.length bs
+        chain (b, bs') = hash $ ByteString.append b $ hashBytes $! getChainHash hash block bs'
+
+hashBytes :: Digest h -> ByteString
+hashBytes = bytestringDigest
+
+----
+
+main = getArgs >>= ByteString.readFile . head >>= print . getHash where
+    getHash = getChainHash sha256 1024
